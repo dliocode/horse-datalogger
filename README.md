52d74f1777750d5724638af5f5d71970b698e64b
Danilo Lucas <developer.dlio@gmail.com>
2022-12-23 08:18:27 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,286 @@
+# Horse-DataLogger
+
+<p align="center">
+  <img src="https://img.shields.io/github/v/release/dliocode/horse-datalogger?style=flat-square">
+  <img src="https://img.shields.io/github/stars/dliocode/horse-datalogger?style=flat-square">
+  <img src="https://img.shields.io/github/forks/dliocode/horse-datalogger?style=flat-square">
+  <img src="https://img.shields.io/github/contributors/dliocode/horse-datalogger?color=orange&style=flat-square">
+  <img src="https://tokei.rs/b1/github/dliocode/horse-datalogger?color=red&category=lines">
+  <img src="https://tokei.rs/b1/github/dliocode/horse-datalogger?color=green&category=code">
+  <img src="https://tokei.rs/b1/github/dliocode/horse-datalogger?color=yellow&category=files">
+</p>
+
+# 
+
+### Horse-DataLogger: Middleware projetado registrar todas às requisições e solicitações HTTP.
+
+Support: developer.dlio@gmail.com
+
+## ⚙️ Instalação
+
+### Para instalar em seu projeto usando [boss](https://github.com/HashLoad/boss):
+```sh
+$ boss install github.com/dliocode/horse-datalogger
+```
+
+### Instalação Manual
+
+Adicione as seguintes pastas ao seu projeto, em *Project > Options > Delphi Compiler > Search path*
+
+```
+../src
+```
+
+### Dependências
+
+[DataLogger](https://github.com/dliocode/datalogger) - Essa é ferramenta utilizado para registrar todas solicitações HTTP do Horse. Para mais informações de como utilizar essa ferramenta em outras situações, [Samples](https://github.com/dliocode/datalogger#providers).
+
+[ClientIP](https://github.com/dliocode/horse-utils-clientip) - Utilizado para capturar o IP.
+
+# Informações
+
+Para usar este Middleware é necessário entender algumas coisas.
+
+## _Providers_ 
+
+Serve essencialmente para armazenar seus logs.
+
+## Quais são os _Providers_ diponíveis
+
+[Clique aqui](https://github.com/dliocode/datalogger#providers)
+
+## Em qual posição é recomendado utilizar este _provider_
+
+Recomendamos que seja adicionado na primeira posição, para que seja registrado todas as informações passadas por ele;
+
+
+# Como Usar
+
+Para utilizar é necessário adicionar a Uses ```Horse.DataLogger``` e depois adicionar os _Providers_ escolhidos para fazer o registro dos logs;
+
+Agora que você já entendeu um pouco de como funciona, vamos aos exemplos;
+
+## Simples
+
+```delphi
+uses 
+  Horse, Horse.DataLogger,
+  DataLogger.Provider.Console; // Provider para Console
+
+begin
+  THorse
+  .Use(THorseDataLogger.Logger([TProviderConsole.Create])) // Adiconando Middleware e o Provider
+
+  .Get('/ping',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      Res.Send('pong');
+    end);
+
+  THorse.Listen(9000);
+end.
+```
+
+## Formatos Predefinidos
+
+``` Combined, Common, Dev, Short, Tiny ```
+
+Cada formato possui uma estrutura diferente e preestabelecida.
+
+### Combined
+
+Saída de log combinada Apache padrão.
+
+```
+${request_remote_clientip} [${time}] "${request_method} ${request_raw_path_info}${request_query} '${request_protocol_version}" ${response_status_code} ${response_content_length} "${request_referer}" "${request_user_agent}"
+```
+
+### Common
+
+Saída de log comum Apache padrão.
+
+```
+${request_remote_clientip} [${time}] "${request_method} ${request_raw_path_info}${request_query} '${request_protocol_version}" ${response_status_code} ${response_content_length}
+```
+
+### Dev
+
+Saída de log simples
+
+```
+${request_method} ${request_raw_path_info}${request_query} ${response_status_code} ${execution_time} ms - ${response_content_length}
+```
+
+### Short
+
+Mais curto que o padrão, incluindo também o tempo de resposta.
+
+```
+${request_remote_clientip} ${request_method} ${request_raw_path_info}${request_query} ${request_protocol_version} ${response_status_code} ${response_content_length} ${execution_time} ms
+```
+
+### Tiny
+
+Saída mínima de log
+
+```
+${request_method} ${request_raw_path_info}${request_query} ${response_status_code} ${response_content_length} - ${execution_time} ms
+```
+
+## Exemplo de uso
+
+```delphi
+uses 
+  Horse, Horse.DataLogger,
+  DataLogger.Provider.Console; // Provider para Console
+
+begin
+  THorse
+  .Use(
+    THorseDataLogger.Logger(
+      THorseDataLoggerFormat.tfCombined, // Formato dos logs  
+      [TProviderConsole.Create]          // Adicionado o Middleware
+    )
+  ) 
+
+  .Get('/ping',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      Res.Send('pong');
+    end);
+
+   // output: 0:0:0:0:0:0:0:1 [2022-12-22 17:18:31:791] "GET /ping HTTP/1.1" 200 4 "-" "PostmanRuntime/7.30.0"
+
+  THorse.Listen(9000);
+end.
+```
+
+## Formatos customizados
+
+Você pode definir seus próprios formatos de utilização
+
+```delphi
+uses 
+  Horse, Horse.DataLogger,
+  DataLogger.Provider.Console; // Provider para Console
+
+begin
+  THorse
+  .Use(
+    THorseDataLogger.Logger(
+      '${request_method} ${request_raw_path_info}${request_query} ${response_status_code} ${response_content_length} - ${execution_time} ms', // Formato dos logs  
+      [TProviderConsole.Create]          // Adicionado o Middleware
+    )
+  ) 
+
+  .Get('/ping',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      Res.Send('pong');
+    end);
+
+   // output: 0:0:0:0:0:0:0:1 [2022-12-22 17:18:31:791] "GET /ping HTTP/1.1" 200 4 "-" "PostmanRuntime/7.30.0"
+
+  THorse.Listen(9000);
+end.
+```
+
+## Formatos disponíveis
+
+```
+${time}
+${execution_time}
+${request_accept}
+${request_authorization}
+${request_cache_control}
+${request_connection}
+${request_content}
+${request_content_encoding}
+${request_content_length}
+${request_content_type}
+${request_content_version}
+${request_cookie}
+${request_cookie_fields}
+${request_derived_from}
+${request_from}
+${request_host}
+${request_internal_path_info}
+${request_internal_script_name}
+${request_method}
+${request_path_info}
+${request_path_translated}
+${request_protocol_version}
+${request_query}
+${request_query_fields}
+${request_raw_path_info}
+${request_referer}
+${request_remote_addr}
+${request_remote_clientip}
+${request_remote_host}
+${request_remote_ip}
+${request_script_name}
+${request_server_port}
+${request_title}
+${request_url}
+${request_user_agent}
+${response_allow}
+${response_content}
+${response_content_encoding}
+${response_content_length}
+${response_content_type}
+${response_content_version}
+${response_custom_headers}
+${response_date}
+${response_derived_from}
+${response_expires}
+${response_last_modified}
+${response_location}
+${response_log_message}
+${response_realm}
+${response_reason}
+${response_server}
+${response_status_code}
+${response_title}
+${response_version}
+${response_wwwauthenticate}
+```
+
+## Adicionando outros _Providers_
+
+Você pode adicionar vários _Providers_ para registrar cada solicitação em locais diferentes.
+
+Para este exemplo, vamos mostrar as requisições em Console e vamos salvar no formato Texto, tudo isso utilizando duas _Units_ para registrar ```   DataLogger.Provider.Console, DataLogger.Provider.TextFile ```.
+
+```delphi
+uses 
+  Horse, Horse.DataLogger,
+  DataLogger.Provider.Console, // Provider para Console
+  DataLogger.Provider.TextFile, // Provider para TextFile
+  System.IOUtils;
+
+begin
+  THorse
+  .Use(
+    THorseDataLogger.Logger(
+      THorseDataLoggerFormat.tfCombined, // Formato dos logs  
+        TProviderConsole.Create,
+      
+        TProviderTextFile.Create
+          .LogDir(TPath.GetDirectoryName(ParamStr(0)) + '\log\request')
+          .PrefixFileName('request_')  
+          .Extension('.txt')      
+      ]          
+    )
+  ) 
+
+  .Get('/ping',
+    procedure(Req: THorseRequest; Res: THorseResponse; Next: TProc)
+    begin
+      Res.Send('pong');
+    end);
+
+   // output: 0:0:0:0:0:0:0:1 [2022-12-22 17:18:31:791] "GET /ping HTTP/1.1" 200 4 "-" "PostmanRuntime/7.30.0"
+
+  THorse.Listen(9000);
+end.
+```
---
+++ src/Horse.DataLogger.pas
@@ -0,0 +1,267 @@
+{
+  ********************************************************************************
+
+  Github - https://github.com/dliocode/horse-datalogger
+
+  ********************************************************************************
+
+  MIT License
+
+  Copyright (c) 2023 Danilo Lucas
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+
+  ********************************************************************************
+}
+
+unit Horse.DataLogger;
+
+interface
+
+uses
+  Horse, Horse.Utils.ClientIP,
+  DataLogger,
+  Web.HTTPApp,
+  System.DateUtils, System.SysUtils, System.JSON, System.NetEncoding;
+
+type
+{$SCOPEDENUMS ON}
+  THorseDataLoggerFormat = (Combined, Common, Dev, Short, Tiny);
+{$SCOPEDENUMS OFF}
+
+  THorseDataLogger = class
+  private
+    class var FDataLogger: TDataLogger;
+    class var FLogFormat: string;
+    class var FLoggerProvider: TArray<TDataLoggerProviderBase>;
+
+    class function ValidateValue(AValue: Int64): string; overload;
+    class function ValidateValue(AValue: string): string; overload;
+    class function ValidateValue(AValue: TDateTime): string; overload;
+
+    class destructor UnInitialize;
+  public
+    class function Logger(const AProvider: TArray<TDataLoggerProviderBase>): THorseCallback; overload;
+    class function Logger(const ALogFormat: THorseDataLoggerFormat; const AProvider: TArray<TDataLoggerProviderBase>): THorseCallback; overload;
+    class function Logger(const ALogFormat: string; const AProvider: TArray<TDataLoggerProviderBase>): THorseCallback; overload;
+  end;
+
+implementation
+
+{ THorseDataLogger }
+
+class function THorseDataLogger.Logger(const AProvider: TArray<TDataLoggerProviderBase>): THorseCallback;
+begin
+  Result := Logger(THorseDataLoggerFormat.Combined, AProvider);
+end;
+
+class function THorseDataLogger.Logger(const ALogFormat: THorseDataLoggerFormat; const AProvider: TArray<TDataLoggerProviderBase>): THorseCallback;
+var
+  LFormat: string;
+begin
+  case ALogFormat of
+    THorseDataLoggerFormat.Combined:
+      LFormat :=
+        '${request_remote_clientip} [${time}] "${request_method} ${request_raw_path_info}${request_query} ' +
+        '${request_protocol_version}" ${response_status_code} ${response_content_length} "${request_referer}" "${request_user_agent}"';
+
+    THorseDataLoggerFormat.Common:
+      LFormat :=
+        '${request_remote_clientip} [${time}] "${request_method} ${request_raw_path_info}${request_query} ' +
+        '${request_protocol_version}" ${response_status_code} ${response_content_length}';
+
+    THorseDataLoggerFormat.Dev:
+      LFormat :=
+        '${request_method} ${request_raw_path_info}${request_query} ' +
+        '${response_status_code} ${execution_time} ms - ${response_content_length}';
+
+    THorseDataLoggerFormat.Short:
+      LFormat :=
+        '${request_remote_clientip} ${request_method} ${request_raw_path_info}${request_query} ' +
+        '${request_protocol_version} ${response_status_code} ${response_content_length} ${execution_time} ms';
+
+    THorseDataLoggerFormat.Tiny:
+      LFormat :=
+        '${request_method} ${request_raw_path_info}${request_query} ' +
+        '${response_status_code} ${response_content_length} - ${execution_time} ms';
+  end;
+
+  Result := Logger(LFormat, AProvider);
+end;
+
+class function THorseDataLogger.Logger(const ALogFormat: string; const AProvider: TArray<TDataLoggerProviderBase>): THorseCallback;
+begin
+  FLogFormat := ALogFormat;
+  FLoggerProvider := AProvider;
+
+  Result :=
+      procedure(ARequest: THorseRequest; AResponse: THorseResponse; ANext: TProc)
+    var
+      LBeforeDateTime: TDateTime;
+      LMilliSecondsBetween: Integer;
+      LWebRequest: TWebRequest;
+      LWebResponse: TWebResponse;
+      LJOMessage: TJSONObject;
+    begin
+      if not Assigned(FDataLogger) then
+      begin
+        FDataLogger := TDataLogger.Builder;
+        FDataLogger.SetProvider(FLoggerProvider);
+        FDataLogger.SetLogFormat(FLogFormat);
+      end;
+
+      LJOMessage := TJSONObject.Create;
+      try
+        LBeforeDateTime := Now;
+        try
+          ANext;
+        finally
+          LMilliSecondsBetween := MilliSecondsBetween(Now, LBeforeDateTime);
+
+          LWebRequest := ARequest.RawWebRequest;
+          LWebResponse := AResponse.RawWebResponse;
+
+          LJOMessage.AddPair('time', ValidateValue(LBeforeDateTime));
+          LJOMessage.AddPair('execution_time', ValidateValue(LMilliSecondsBetween));
+
+          // Request
+          LJOMessage.AddPair('request_accept', ValidateValue(LWebRequest.Accept));
+          LJOMessage.AddPair('request_authorization', ValidateValue(LWebRequest.Authorization));
+          LJOMessage.AddPair('request_cache_control', ValidateValue(LWebRequest.CacheControl));
+          LJOMessage.AddPair('request_connection', ValidateValue(LWebRequest.Connection));
+
+          try
+            if not Trim(LWebRequest.ContentType).Contains('multipart/form-data') then
+              LJOMessage.AddPair('request_content', ValidateValue(LWebRequest.Content));
+          except
+            LJOMessage.AddPair('request_content', ValidateValue(''));
+          end;
+
+          LJOMessage.AddPair('request_content_encoding', ValidateValue(LWebRequest.ContentEncoding));
+          LJOMessage.AddPair('request_content_length', ValidateValue(LWebRequest.ContentLength));
+          LJOMessage.AddPair('request_content_type', ValidateValue(LWebRequest.ContentType));
+          LJOMessage.AddPair('request_content_version', ValidateValue(LWebRequest.ContentVersion));
+          LJOMessage.AddPair('request_cookie', ValidateValue(LWebRequest.Cookie));
+          LJOMessage.AddPair('request_cookie_fields', ValidateValue(LWebRequest.CookieFields.Text));
+          LJOMessage.AddPair('request_derived_from', ValidateValue(LWebRequest.DerivedFrom));
+          LJOMessage.AddPair('request_from', ValidateValue(LWebRequest.From));
+          LJOMessage.AddPair('request_host', ValidateValue(LWebRequest.Host));
+          LJOMessage.AddPair('request_internal_path_info', ValidateValue(LWebRequest.InternalPathInfo));
+          LJOMessage.AddPair('request_internal_script_name', ValidateValue(LWebRequest.InternalScriptName));
+          LJOMessage.AddPair('request_method', ValidateValue(LWebRequest.Method));
+          LJOMessage.AddPair('request_path_info', ValidateValue(LWebRequest.PathInfo));
+          LJOMessage.AddPair('request_path_translated', ValidateValue(LWebRequest.PathTranslated));
+          LJOMessage.AddPair('request_protocol_version', ValidateValue(LWebRequest.ProtocolVersion));
+
+          if Trim(LWebRequest.Query).IsEmpty then
+            LJOMessage.AddPair('request_query', '')
+          else
+            LJOMessage.AddPair('request_query', '?' + ValidateValue(LWebRequest.Query));
+
+          LJOMessage.AddPair('request_query_fields', ValidateValue(LWebRequest.QueryFields.Text));
+          LJOMessage.AddPair('request_raw_path_info', ValidateValue(LWebRequest.RawPathInfo));
+          LJOMessage.AddPair('request_referer', ValidateValue(LWebRequest.Referer));
+          LJOMessage.AddPair('request_remote_addr', ValidateValue(LWebRequest.RemoteAddr));
+          LJOMessage.AddPair('request_remote_clientip', ValidateValue(ClientIP(ARequest)));
+          LJOMessage.AddPair('request_remote_host', ValidateValue(LWebRequest.RemoteHost));
+          LJOMessage.AddPair('request_remote_ip', ValidateValue(LWebRequest.RemoteIP));
+          LJOMessage.AddPair('request_script_name', ValidateValue(LWebRequest.ScriptName));
+          LJOMessage.AddPair('request_server_port', ValidateValue(LWebRequest.ServerPort));
+          LJOMessage.AddPair('request_title', ValidateValue(LWebRequest.Title));
+          LJOMessage.AddPair('request_url', ValidateValue(LWebRequest.URL));
+          LJOMessage.AddPair('request_user_agent', ValidateValue(LWebRequest.UserAgent));
+
+          // Response
+          LJOMessage.AddPair('response_allow', ValidateValue(LWebResponse.Allow));
+          LJOMessage.AddPair('response_content', ValidateValue(LWebResponse.Content));
+          LJOMessage.AddPair('response_content_encoding', ValidateValue(LWebResponse.ContentEncoding));
+          LJOMessage.AddPair('response_content_length', ValidateValue(LWebResponse.ContentLength));
+          LJOMessage.AddPair('response_content_type', ValidateValue(LWebResponse.ContentType));
+          LJOMessage.AddPair('response_content_version', ValidateValue(LWebResponse.ContentVersion));
+          LJOMessage.AddPair('response_custom_headers', ValidateValue(LWebResponse.CustomHeaders.Text));
+          LJOMessage.AddPair('response_date', ValidateValue(LWebResponse.Date));
+          LJOMessage.AddPair('response_derived_from', ValidateValue(LWebResponse.DerivedFrom));
+          LJOMessage.AddPair('response_expires', ValidateValue(LWebResponse.Expires));
+          LJOMessage.AddPair('response_last_modified', ValidateValue(LWebResponse.LastModified));
+          LJOMessage.AddPair('response_location', ValidateValue(LWebResponse.Location));
+          LJOMessage.AddPair('response_log_message', ValidateValue(LWebResponse.LogMessage));
+          LJOMessage.AddPair('response_realm', ValidateValue(LWebResponse.Realm));
+          LJOMessage.AddPair('response_reason', ValidateValue(LWebResponse.ReasonString));
+          LJOMessage.AddPair('response_server', ValidateValue(LWebResponse.Server));
+          LJOMessage.AddPair('response_status_code', ValidateValue(LWebResponse.StatusCode));
+          LJOMessage.AddPair('response_title', ValidateValue(LWebResponse.Title));
+          LJOMessage.AddPair('response_version', ValidateValue(LWebResponse.Version));
+          LJOMessage.AddPair('response_wwwauthenticate', ValidateValue(LWebResponse.WWWAuthenticate));
+
+          case AResponse.Status of
+            0 .. 299:
+              FDataLogger.Success(LJOMessage, ClassName);
+
+            300 .. 399:
+              FDataLogger.Info(LJOMessage, ClassName);
+
+            400 .. 499:
+              FDataLogger.Warn(LJOMessage, ClassName);
+          else
+            FDataLogger.Error(LJOMessage, ClassName);
+          end;
+        end;
+      finally
+        LJOMessage.Free;
+      end;
+    end;
+end;
+
+class destructor THorseDataLogger.UnInitialize;
+var
+  I: Integer;
+begin
+  if Assigned(FDataLogger) then
+  begin
+    FDataLogger.Free;
+    FDataLogger := nil;
+  end
+  else
+    for I := Low(FLoggerProvider) to High(FLoggerProvider) do
+      FLoggerProvider[I].Free
+end;
+
+class function THorseDataLogger.ValidateValue(AValue: Int64): string;
+begin
+  Result := AValue.ToString;
+end;
+
+class function THorseDataLogger.ValidateValue(AValue: string): string;
+var
+  LMessage: string;
+begin
+  LMessage := String(UTF8Encode(AValue));
+
+  if LMessage.IsEmpty then
+    Result := '-'
+  else
+    Result := LMessage;
+end;
+
+class function THorseDataLogger.ValidateValue(AValue: TDateTime): string;
+begin
+  Result := FormatDateTime('yyyy-mm-dd hh:mm:ss:zzz', AValue);
+end;
+
+end.
